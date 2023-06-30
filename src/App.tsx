9c34b54402fb82b8fe7ec1c8fb0af085c36f8010
HM22-code <H.M-22@outlook.fr>
2023-06-30 16:24:02 +0200

    install bulma css

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,40 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
+		<div className="column">
+			<nav className="navbar" role="navigation" aria-label="main navigation">
+				<div className="navbar-brand">
+					<a className="navbar-item" href="https://bulma.io">
+						<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28" />
+					</a>
+					<a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</a>
+				</div>
+			</nav>
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
 				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
+					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
+			<div className="block">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
+					Edit <code className="tag">src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
-		</>
+		</div>
 	);
 }
 

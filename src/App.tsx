039e47554f65986c846c4f84d4e3b4cb1500504e
HM22-code <H.M-22@outlook.fr>
2023-07-09 13:53:32 +0200

    Fix assets import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import profileLogo from "/assets/img/profile-logo.png";
+import profilePicture from "/assets/img/profile-picture.jpg";
 import reactLogo from "/assets/svg/react.svg";
 import viteLogo from "/assets/svg/vite.svg";
+import bulmaLogo from "/assets/img/bulma-logo.png";
+import cv from "/assets/pdf/cv.pdf";
 import "./App.css";
 import Icon from '@mdi/react';
 import { mdiDownload } from '@mdi/js';
@@ -11,9 +15,7 @@ function App() {
 			<nav className="navbar" role="navigation" aria-label="main navigation">
 				<div className="navbar-brand">
 					<a className="navbar-item">
-					
-					<img src="/assets/img/profile-logo.png" />
-				
+						<img src={profileLogo} />
 					</a>
 					<a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
 						<span aria-hidden="true"></span>
@@ -23,37 +25,39 @@ function App() {
 				</div>
 				<div id="navbarBasicExample" className="navbar-menu">
 					<div className="navbar-start">
-					<a className="navbar-item">
-						About
-					</a>
-					<a className="navbar-item">
-						Resume
-					</a>
-					<a className="navbar-item">
-						Projects
-					</a>
-					<div className="navbar-item has-dropdown is-hoverable">
-						<a className="navbar-link">
-							More
+						<a className="navbar-item">
+							About
 						</a>
-						<div className="navbar-dropdown">
-							<a className="navbar-item">
-								Social
+						<a className="navbar-item">
+							Resume
+						</a>
+						<a className="navbar-item">
+							Projects
+						</a>
+						<div className="navbar-item has-dropdown is-hoverable">
+							<a className="navbar-link">
+								More
 							</a>
-							<a className="navbar-item">
-								Contact
-							</a>
+							<div className="navbar-dropdown">
+								<a className="navbar-item">
+									Social
+								</a>
+								<a className="navbar-item">
+									Contact
+								</a>
+							</div>
 						</div>
-					</div>
 					</div>
 					<div className="navbar-end">
 						<div className="navbar-item">
-							<a href="/assets/pdf/cv.pdf" download className="button is-primary">
+							<a href={cv} download className="button is-primary">
 								<span className="icon-text">
 									<span className="icon">
-									<Icon path={mdiDownload} title="Download CV" size={1} />
+										<Icon path={mdiDownload} title="Download CV" size={1} />
 									</span>
-									<span>Download CV</span>
+									<span>
+										Download CV
+									</span>
 								</span>
 							</a>
 						</div>
@@ -63,24 +67,24 @@ function App() {
 			<section className="hero section">
 				<div className="columns">
 					<div className="column is-one-fifth">
-				<figure className="image is-fullwidth">
-					<img className="is-rounded" src="/assets/img/profile-picture.jpg" />
-				</figure>
-				</div>
-				<div className="column section">
-
-				
-				<h2 className="title is-3">
-					Hi, I’m Hugo 👋
-				</h2>
-				<p className="subtitle">
-					Welcome to my portfolio ! 
-				</p>
-				</div>
+						<figure className="image is-fullwidth">
+							<img className="is-rounded" src={profilePicture} />
+						</figure>
+					</div>
+					<div className="column section">
+						<h2 className="title is-3">
+							Hi, I’m Hugo 👋
+						</h2>
+						<p className="subtitle">
+							Welcome to my portfolio ! 
+						</p>
+					</div>
 				</div>
 			</section>
 			<section className="section">
-				<h2 className="title is-4">About me</h2>
+				<h2 className="title is-4">
+					About me
+				</h2>
 				<p className="has-text-justified">
 					My name's Hugo. I'm a full-stack developer. Currently, 
 					as a student I'm currently studying for a Bachelor's degree in Application Design and Development in Dijon, 
@@ -91,36 +95,44 @@ function App() {
 				</p>
 			</section>
 			<section className="section">
-				<h2 className="title is-4">Resume</h2>
+				<h2 className="title is-4">
+					Resume
+				</h2>
 				<p></p>
 			</section>
 			<section className="section">
-				<h2 className="title is-4">Projects</h2>
+				<h2 className="title is-4">
+					Projects
+				</h2>
 				<p></p>
 			</section>
 			<section className="section">
-				<h2 className="title is-4">Social</h2>
+				<h2 className="title is-4">
+					Social
+				</h2>
 				<p></p>
 			</section>
 			<section className="section">
-				<h2 className="title is-4">Contact</h2>
+				<h2 className="title is-4">
+					Contact
+				</h2>
 				<p></p>
 			</section>
 			<footer className="footer">
-			<div className="level">
-				<p className="level-item">Made with 💙 by HM22-code</p>
-			</div>
-			<div className="level">
-				<a href="https://vitejs.dev" target="_blank" className="level-item">
-					<img src={viteLogo} className="image logo vite" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" className="level-item">
-					<img src={reactLogo} className="image logo react" alt="React logo" />
-				</a>
-				<a href="https://bulma.io" className="level-item" >
-					<img src="/assets/img/bulma-logo.png" className="image" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28" />
-				</a>
-			</div>
+				<div className="level">
+					<p className="level-item">Made with 💙 by HM22-code</p>
+				</div>
+				<div className="level">
+					<a href="https://vitejs.dev" target="_blank" className="level-item">
+						<img src={viteLogo} className="image logo vite" alt="Vite logo" />
+					</a>
+					<a href="https://react.dev" target="_blank" className="level-item">
+						<img src={reactLogo} className="image logo react" alt="React logo" />
+					</a>
+					<a href="https://bulma.io" className="level-item" >
+						<img src={bulmaLogo} className="image" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28" />
+					</a>
+				</div>
 			</footer>
 		</div>
 	);

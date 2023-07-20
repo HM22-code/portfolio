5e2a2828d5cdd003d843c518d7b0e85c1df656d5
HM22-code <H.M-22@outlook.fr>
2023-07-20 23:20:51 +0200

    Update Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,31 @@
 
 function Projects() {
-  return (
-    <section className="section">
-        <h2 className="title is-4">
-            Projects
-        </h2>
-        <p></p>
-    </section>
-  )
+    
+    return (
+        <section className="section">
+            <h2 className="title is-4">
+                Projects
+            </h2>
+            <div className="tile is-vertical is-ancestor">
+                <div className="tile is-parent is-vertical">
+                    <div className="tile is-child box">
+                    <p className="title">One</p>
+                    </div>
+                    <div className="tile is-child box">
+                    <p className="title">Two</p>
+                    </div>
+                </div>
+                <div className="tile is-parent is-vertical">
+                    <div className="tile is-child box">
+                    <p className="title">Three</p>
+                    </div>
+                    <div className="tile is-child box">
+                    <p className="title">Four</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Projects;

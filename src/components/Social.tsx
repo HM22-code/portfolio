98ffaa5624daa7a02106317b065c19623207c385
HM22-code <H.M-22@outlook.fr>
2023-07-20 23:21:18 +0200

    Update Social component

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,12 +1,17 @@
+import Icon from '@mdi/react';
+import { mdiLinkedin } from '@mdi/js';
 
 function Social() {
-  return (
-    <section className="section">
-        <h2 className="title is-4">
-            Social
-        </h2>
-        <p></p>
-    </section>
+    
+    return (
+        <section className="section">
+            <h2 className="title is-4">
+                Social
+            </h2>
+            <a href="https://www.linkedin.com/in/hugo-montandon/">
+                <Icon path={mdiLinkedin} title="Linkedin" size={1} />
+            </a>
+        </section>
   )
 }
 

0d86696a5c81b657deca77277632de832613bcf2
HM22-code <H.M-22@outlook.fr>
2023-07-20 23:20:35 +0200

    Update Banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,25 +2,25 @@ import profilePicture from "/assets/img/profile-picture.jpg";
 
 function Banner() {
     
-  return (
-    <section className="hero section">
-        <div className="columns">
-            <div className="column is-one-fifth">
-                <figure className="image is-fullwidth">
-                    <img className="is-rounded" src={profilePicture} />
-                </figure>
+    return (
+        <section className="hero section">
+            <div className="columns">
+                <div className="column is-one-fifth">
+                    <figure className="image is-fullwidth">
+                        <img className="is-rounded" src={profilePicture} />
+                    </figure>
+                </div>
+                <div className="column section">
+                    <h2 className="title is-3">
+                        Hi, I’m Hugo 👋
+                    </h2>
+                    <p className="subtitle">
+                        Welcome to my portfolio ! 
+                    </p>
+                </div>
             </div>
-            <div className="column section">
-                <h2 className="title is-3">
-                    Hi, I’m Hugo 👋
-                </h2>
-                <p className="subtitle">
-                    Welcome to my portfolio ! 
-                </p>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Banner;

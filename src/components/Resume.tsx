680fdc576d2014e9c615670ace4865212d1caeb2
HM22-code <H.M-22@outlook.fr>
2023-07-20 23:21:04 +0200

    Update Resume component

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,91 +1,91 @@
 
 function Resume() {
 
-  return (
-    <section className="section">
-        <h2 className="title is-4">
-            Resume
-        </h2>
-        <div className="columns">
-            <div className="column">
-                <div className="timeline">
-                    <header className="timeline-header">
-                        <span className="tag is-medium is-primary">Start</span>
-                    </header>
-                    <div className="timeline-item">
-                        <div className="timeline-marker"></div>
-                        <div className="timeline-content">
-                            <p className="heading">January 2016</p>
-                            <p>Timeline content - Can include any HTML element</p>
+    return (
+        <section className="section">
+            <h2 className="title is-4">
+                Resume
+            </h2>
+            <div className="columns">
+                <div className="column">
+                    <div className="timeline">
+                        <header className="timeline-header">
+                            <span className="tag is-medium is-primary">Start</span>
+                        </header>
+                        <div className="timeline-item">
+                            <div className="timeline-marker"></div>
+                            <div className="timeline-content">
+                                <p className="heading">January 2016</p>
+                                <p>Timeline content - Can include any HTML element</p>
+                            </div>
+                        </div>
+                        <div className="timeline-item">
+                            <div className="timeline-marker is-image is-32x32">
+                                <img src="https://bulma.io/images/placeholders/32x32.png" />
+                            </div>
+                            <div className="timeline-content">
+                                <p className="heading">February 2016</p>
+                                <p>Timeline content - Can include any HTML element</p>
+                            </div>
+                        </div>
+                        <header className="timeline-header">
+                            <span className="tag is-primary">2017</span>
+                        </header>
+                        <div className="timeline-item">
+                            <div className="timeline-marker is-icon">
+                                <i className="fa fa-flag"></i>
+                            </div>
+                            <div className="timeline-content">
+                                <p className="heading">March 2017</p>
+                                <p>Timeline content - Can include any HTML element</p>
+                            </div>
+                        </div>
+                        <div className="timeline-header">
+                            <span className="tag is-medium is-primary">End</span>
                         </div>
                     </div>
-                    <div className="timeline-item">
-                        <div className="timeline-marker is-image is-32x32">
-                            <img src="https://bulma.io/images/placeholders/32x32.png" />
+                </div>
+                <div className="column">
+                    <div className="timeline">
+                        <header className="timeline-header">
+                            <span className="tag is-medium is-primary">Start</span>
+                        </header>
+                        <div className="timeline-item">
+                            <div className="timeline-marker"></div>
+                            <div className="timeline-content">
+                                <p className="heading">January 2016</p>
+                                <p>Timeline content - Can include any HTML element</p>
+                            </div>
                         </div>
-                        <div className="timeline-content">
-                            <p className="heading">February 2016</p>
-                            <p>Timeline content - Can include any HTML element</p>
+                        <div className="timeline-item">
+                            <div className="timeline-marker is-image is-32x32">
+                                <img src="https://bulma.io/images/placeholders/32x32.png" />
+                            </div>
+                            <div className="timeline-content">
+                                <p className="heading">February 2016</p>
+                                <p>Timeline content - Can include any HTML element</p>
+                            </div>
                         </div>
-                    </div>
-                    <header className="timeline-header">
-                        <span className="tag is-primary">2017</span>
-                    </header>
-                    <div className="timeline-item">
-                        <div className="timeline-marker is-icon">
-                            <i className="fa fa-flag"></i>
+                        <header className="timeline-header">
+                            <span className="tag is-primary">2017</span>
+                        </header>
+                        <div className="timeline-item">
+                            <div className="timeline-marker is-icon">
+                                <i className="fa fa-flag"></i>
+                            </div>
+                            <div className="timeline-content">
+                                <p className="heading">March 2017</p>
+                                <p>Timeline content - Can include any HTML element</p>
+                            </div>
                         </div>
-                        <div className="timeline-content">
-                            <p className="heading">March 2017</p>
-                            <p>Timeline content - Can include any HTML element</p>
+                        <div className="timeline-header">
+                            <span className="tag is-medium is-primary">End</span>
                         </div>
-                    </div>
-                    <div className="timeline-header">
-                        <span className="tag is-medium is-primary">End</span>
                     </div>
                 </div>
             </div>
-            <div className="column">
-                <div className="timeline">
-                    <header className="timeline-header">
-                        <span className="tag is-medium is-primary">Start</span>
-                    </header>
-                    <div className="timeline-item">
-                        <div className="timeline-marker"></div>
-                        <div className="timeline-content">
-                            <p className="heading">January 2016</p>
-                            <p>Timeline content - Can include any HTML element</p>
-                        </div>
-                    </div>
-                    <div className="timeline-item">
-                        <div className="timeline-marker is-image is-32x32">
-                            <img src="https://bulma.io/images/placeholders/32x32.png" />
-                        </div>
-                        <div className="timeline-content">
-                            <p className="heading">February 2016</p>
-                            <p>Timeline content - Can include any HTML element</p>
-                        </div>
-                    </div>
-                    <header className="timeline-header">
-                        <span className="tag is-primary">2017</span>
-                    </header>
-                    <div className="timeline-item">
-                        <div className="timeline-marker is-icon">
-                            <i className="fa fa-flag"></i>
-                        </div>
-                        <div className="timeline-content">
-                            <p className="heading">March 2017</p>
-                            <p>Timeline content - Can include any HTML element</p>
-                        </div>
-                    </div>
-                    <div className="timeline-header">
-                        <span className="tag is-medium is-primary">End</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Resume;

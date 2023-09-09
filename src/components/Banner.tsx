93b4fc99413502bdb6fae2e5853690e9a1a3c9ce
HM22-code <H.M-22@outlook.fr>
2023-09-09 23:35:14 +0200

    Update Banner section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,15 @@
 import profilePicture from "/assets/img/profile-picture.jpg";
+import Icon from '@mdi/react';
+import { mdiLinkedin } from '@mdi/js';
 
 function Banner() {
-    
+
     return (
-        <section className="hero section">
+        <section className="section hero">
             <div className="columns">
                 <div className="column is-one-fifth">
                     <figure className="image is-fullwidth">
-                        <img className="is-rounded" src={profilePicture} />
+                        <img className="is-rounded " src={profilePicture} />
                     </figure>
                 </div>
                 <div className="column section">
@@ -17,6 +19,7 @@ function Banner() {
                     <p className="subtitle">
                         Welcome to my portfolio ! 
                     </p>
+                    <div className="badge-base LI-profile-badge" data-locale="fr_FR" data-size="medium" data-theme="light" data-type="HORIZONTAL" data-vanity="hugo-montandon" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://fr.linkedin.com/in/hugo-montandon?trk=profile-badge"><Icon path={mdiLinkedin} title="Linkedin" size={1} />LinkedIn Profile</a></div>
                 </div>
             </div>
         </section>

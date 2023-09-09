1df8cedbbd254aa2e08e8a0c1291f28ce4d6cedc
HM22-code <H.M-22@outlook.fr>
2023-09-09 23:36:29 +0200

    Update Social section

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,16 +1,24 @@
 import Icon from '@mdi/react';
 import { mdiLinkedin } from '@mdi/js';
+import { mdiGithub } from '@mdi/js';
+import { mdiGmail } from '@mdi/js';
 
 function Social() {
     
     return (
-        <section className="section">
+        <section id="social" className="section">
             <h2 className="title is-4">
                 Social
             </h2>
             <a href="https://www.linkedin.com/in/hugo-montandon/">
-                <Icon path={mdiLinkedin} title="Linkedin" size={1} />
+                <Icon path={mdiLinkedin} title="Linkedin" size={2} />
             </a>
+            <a href="https://github.com/HM22-code">
+                <Icon path={mdiGithub} title="Linkedin" size={2} />
+            </a>
+            <a href="mailto:hugo.montandon2@gmail.com">
+                <Icon path={mdiGmail} title="Linkedin" size={2} />
+            </a>          
         </section>
   )
 }

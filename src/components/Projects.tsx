592516158668ca2ffe16101341aa4238ca2ae345
HM22-code <H.M-22@outlook.fr>
2023-09-09 23:36:04 +0200

    Update Project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,25 +2,97 @@
 function Projects() {
     
     return (
-        <section className="section">
+        <section id="projects" className="section">
             <h2 className="title is-4">
                 Projects
             </h2>
             <div className="tile is-vertical is-ancestor">
                 <div className="tile is-parent is-vertical">
-                    <div className="tile is-child box">
-                    <p className="title">One</p>
+                    <div className="tile is-child box card">
+                        <div className="card-image">
+                            <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                            </figure>
+                        </div>
+                        <div className="card-content">
+                            <div className="media">
+                                <div className="media-content">
+                                    <p className="title is-4">Title</p>
+                                    <p className="subtitle is-6">Subtitle</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris.
+                            </div>
+                            <br/>
+                            <a className="button is-primary">Source Code</a>
+                        </div>
                     </div>
-                    <div className="tile is-child box">
-                    <p className="title">Two</p>
+                    <div className="tile is-child box card">
+                        <div className="card-image">
+                            <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                            </figure>
+                        </div>
+                        <div className="card-content">
+                            <div className="media">
+                                <div className="media-content">
+                                    <p className="title is-4">Title</p>
+                                    <p className="subtitle is-6">Subtitle</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris.
+                            </div>
+                            <br/>
+                            <a className="button is-primary">Source Code</a>
+                        </div>
                     </div>
                 </div>
                 <div className="tile is-parent is-vertical">
-                    <div className="tile is-child box">
-                    <p className="title">Three</p>
+                    <div className="tile is-child box card">
+                        <div className="card-image">
+                            <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                            </figure>
+                        </div>
+                        <div className="card-content">
+                            <div className="media">
+                                <div className="media-content">
+                                    <p className="title is-4">Title</p>
+                                    <p className="subtitle is-6">Subtitle</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris.
+                            </div>
+                            <br/>
+                            <a className="button is-primary">Source Code</a>
+                        </div>
                     </div>
-                    <div className="tile is-child box">
-                    <p className="title">Four</p>
+                    <div className="tile is-child box card">
+                        <div className="card-image">
+                            <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                            </figure>
+                        </div>
+                        <div className="card-content">
+                            <div className="media">
+                                <div className="media-content">
+                                    <p className="title is-4">Title</p>
+                                    <p className="subtitle is-6">Subtitle</p>
+                                </div>
+                            </div>
+                            <div className="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris.
+                            </div>
+                            <br/>
+                            <a className="button is-primary">Source Code</a>
+                        </div>
                     </div>
                 </div>
             </div>

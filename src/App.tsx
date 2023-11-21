e8a2be866962bdd84b53eb2b1246526545ae7e9c
HM22-code <H.M-22@outlook.fr>
2023-11-21 20:17:19 +0100

    Adding Loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,37 @@ import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import Social from "./components/Social";
 import Footer from "./components/Footer";
+import Loading from "./components/Loading";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         AOS.init();
+        setTimeout(() => setLoading(false), 3000);
     }, [])
 
 	return (
 		<>
-			<Navbar />
-			<Banner />
-            <Social />
-			<About />
-			<Resume />
-			<Projects />
-			<Footer />	
+        {
+            loading === false ? (
+            <>
+                <Navbar />
+                <Banner />
+                <Social />
+                <About />
+                <Resume />
+                <Projects />
+                <Footer />
+            </>	
+            ) : (
+            <Loading />
+            )
+        }
 		</>
 	);
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+
+function Loading() {
+
+    return (
+        <div className="hero is-fullheight">
+            <div className="hero-body">
+                <progress className="progress is-large is-primary" max="100">60%</progress>
+            </div>
+        </div>
+    );
+}
+
+export default Loading;

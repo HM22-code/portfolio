09e1b9b6d6075fff491ce90b48d324e124cac3b6
HM22-code <H.M-22@outlook.fr>
2024-07-03 11:52:38 +0200

    Adding ProjectCard component

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,39 @@
+type Props = {
+    preview: string;
+    link: string;
+    title: string;
+    subtitle: string;
+    description: string;
+}
+
+function ProjectCard(props : Props) {
+    
+    return (
+        <div className="column m-1 box card"
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+        >
+            <a href={props.link} className="card-image">
+                <figure className="image is-4by3">
+                    <img src={props.preview} alt="Project preview" height="960" width="1280"/>
+                </figure>
+            </a>
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">{props.title}</p>
+                        <p className="subtitle is-6">{props.subtitle}</p>
+                    </div>
+                </div>
+                <div className="content">
+                    {props.description}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,4 @@
+import ProjectCard from "./ProjectCard";
 import project1 from "/assets/img/project-1.png";
 import project2 from "/assets/img/project-2.png";
 import project3 from "/assets/img/project-3.png";
@@ -20,82 +21,27 @@ function Projects() {
             </h2>
             <br/>
             <div className="columns is-desktop is-multiline">
-                
-                <div className="column m-1 box card"
-                    data-aos="fade-up"
-                    data-aos-offset="200"
-                    data-aos-delay="50"
-                    data-aos-duration="1000"
-                    data-aos-easing="ease-in-out"
-                >
-                    <a href="https://github.com/HM22-code/RPG-idea" className="card-image">
-                        <figure className="image is-4by3">
-                            <img src={project1} alt="Project preview" height="960" width="1280"/>
-                        </figure>
-                    </a>
-                    <div className="card-content">
-                        <div className="media">
-                            <div className="media-content">
-                                <p className="title is-4">{t('projects.project1')}</p>
-                                <p className="subtitle is-6">{t('projects.subtitle1')}</p>
-                            </div>
-                        </div>
-                        <div className="content">
-                            {t('projects.description1')}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="column m-1 box card"
-                    data-aos="fade-up"
-                    data-aos-offset="200"
-                    data-aos-delay="50"
-                    data-aos-duration="1000"
-                    data-aos-easing="ease-in-out"
-                >
-                    <a href="https://hm22-code.github.io/static-web-page-showcase/" className="card-image">
-                        <figure className="image is-4by3">
-                            <img src={project2} alt="Project preview" height="960" width="1280"/>
-                        </figure>
-                    </a>
-                    <div className="card-content">
-                        <div className="media">
-                            <div className="media-content">
-                                <p className="title is-4">{t('projects.project2')}</p>
-                                <p className="subtitle is-6">{t('projects.subtitle2')}</p>
-                            </div>
-                        </div>
-                        <div className="content">
-                            {t('projects.description2')}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="column m-1 box card"
-                    data-aos="fade-up"
-                    data-aos-offset="200"
-                    data-aos-delay="50"
-                    data-aos-duration="1000"
-                    data-aos-easing="ease-in-out"
-                >
-                    <a href="https://jude-erdrick.itch.io/ex-raceone" className="card-image">
-                        <figure className="image is-4by3">
-                            <img src={project3} alt="Project preview" height="960" width="1280"/>
-                        </figure>
-                    </a>
-                    <div className="card-content">
-                        <div className="media">
-                            <div className="media-content">
-                                <p className="title is-4">{t('projects.project3')}</p>
-                                <p className="subtitle is-6">{t('projects.subtitle3')}</p>
-                            </div>
-                        </div>
-                        <div className="content">
-                            {t('projects.description3')}
-                        </div>
-                    </div>
-                </div>
-
+                <ProjectCard 
+                    preview={project1}
+                    link="https://github.com/HM22-code/RPG-idea"
+                    title={t('projects.project1')} 
+                    subtitle={t('projects.subtitle1')} 
+                    description={t('projects.description1')}
+                />
+                <ProjectCard 
+                    preview={project2}
+                    link="https://hm22-code.github.io/static-web-page-showcase/"
+                    title={t('projects.project2')} 
+                    subtitle={t('projects.subtitle2')} 
+                    description={t('projects.description2')}
+                />
+                <ProjectCard 
+                    preview={project3}
+                    link="https://jude-erdrick.itch.io/ex-raceone"
+                    title={t('projects.project3')} 
+                    subtitle={t('projects.subtitle3')} 
+                    description={t('projects.description3')}
+                />
             </div>
         </section>
     )

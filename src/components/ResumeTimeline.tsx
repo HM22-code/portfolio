f4cf84eace837bd731fc15b36fe5c7ba73c14f76
HM22-code <H.M-22@outlook.fr>
2024-07-05 15:28:18 +0200

    Fix tag flex behaviour

--- src/components/ResumeTimeline.tsx
+++ src/components/ResumeTimeline.tsx
@@ -28,16 +28,16 @@ function ResumeTimeline(props: ResumeTimelineProps) {
                             <p className="heading">{item.period}</p>
                             <p className="is-size-5 has-text-weight-bold">{item.work}</p>
                             <p className="is-italic is-size-8">{item.location}</p>
-                            <div className="level is-mobile">
-                                <div className="level-left is-mobile">
+                            
+                                <div className="level-left is-mobile is-flex-direction-row is-flex-wrap-wrap is-flex">
                                     {
                                         item.keywords.split(".").map((item, key) =>
-                                            <div className="level-item mr-1">
+                                            <div className="level-item mr-1 mb-1">
                                                 <span key={key} className="tag is-primary">{item}</span>
                                             </div>
                                         )
                                     }
-                                </div>
+                                
                             </div>
                         </div>
                     </div>

f5ce70eb639ef1a6b040794e9d848966ac5282de
HM22-code <83293398+HM22-code@users.noreply.github.com>
2025-12-01 23:19:13 +0100

    Update resume timeline component

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -19,33 +19,40 @@ function Resume() {
             <div className="columns">
 
                 <div className="column">
-                    <h3 className="subtitle is-6 is-underlined">{t('resume.subtitle1')}</h3>
-                    <ResumeTimeline items={[{
-                        period: t('resume.period1'),
-                        work: t('resume.work1'),
-                        location: t('resume.location1'),
-                        keywords: t('resume.keywords1')
-                    }, {
-                        period: t('resume.period2'),
-                        work: t('resume.work2'),
-                        location: t('resume.location2'),
-                        keywords: t('resume.keywords2')
-                    }]}/>
+                    <ResumeTimeline items={
+                        [
+                            {
+                                period: t('resume.period4'),
+                                work: t('resume.work4'),
+                                location: t('resume.location4'),
+                                keywords: t('resume.keywords4')
+                            },
+                            {
+                                period: t('resume.period3'),
+                                work: t('resume.work3'),
+                                location: t('resume.location3'),
+                                keywords: t('resume.keywords3')
+                            },
+                            {
+                                period: t('resume.period2'),
+                                work: t('resume.work2'),
+                                location: t('resume.location2'),
+                                keywords: t('resume.keywords2')
+                            },
+                            {
+                                period: t('resume.period1'),
+                                work: t('resume.work1'),
+                                location: t('resume.location1'),
+                                keywords: t('resume.keywords1')
+                            }
+                        ]
+                    }/>
                 </div>
 
                 <div className="column">
-                    <h3 className="subtitle is-6 is-underlined">{t('resume.subtitle2')}</h3>
-                    <ResumeTimeline items={[{
-                        period: t('resume.period3'),
-                        work: t('resume.work3'),
-                        location: t('resume.location3'),
-                        keywords: t('resume.keywords3')
-                    }, {
-                        period: t('resume.period4'),
-                        work: t('resume.work4'),
-                        location: t('resume.location4'),
-                        keywords: t('resume.keywords4')
-                    }]}/>
+                    {
+                        /* TODO: place for additional content */
+                    }
                 </div>
             </div>
         </section>
--- src/components/ResumeTimeline.tsx
+++ src/components/ResumeTimeline.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 
 export interface TimelineItem {
     period: string;
@@ -12,14 +11,8 @@ type ResumeTimelineProps = {
 }
 
 function ResumeTimeline(props: ResumeTimelineProps) {
-
-    const { t } = useTranslation();
-
     return (
         <div className="timeline">
-            <header className="timeline-header">
-                <span className="tag is-medium">{t('resume.start')}</span>
-            </header>
             {
                 props.items.map((item: TimelineItem, key) =>
                     <div key={key} className="timeline-item">
@@ -28,7 +21,7 @@ function ResumeTimeline(props: ResumeTimelineProps) {
                             <p className="heading">{item.period}</p>
                             <p className="is-size-5 has-text-weight-bold">{item.work}</p>
                             <p className="is-italic is-size-8">{item.location}</p>
-                            <div className="level-left is-mobile is-flex-direction-row is-flex-wrap-wrap is-flex">
+                            <div className="level-left is-mobile is-flex-direction-row is-flex-wrap-wrap is-flex pt-2">
                                 {
                                     item.keywords.split(".").map((item, key) =>
                                         <div className="level-item mr-1 mb-1">
@@ -41,11 +34,7 @@ function ResumeTimeline(props: ResumeTimelineProps) {
                     </div>
                 )
             }
-            <div className="timeline-header">
-                <span className="tag is-medium">{t('resume.end')}</span>
-            </div>
         </div>
     )
 }
-
 export default ResumeTimeline;

dca2da05f4f0021e7f8fba0e8519dbf23e7f4f77
HM22-code <83293398+HM22-code@users.noreply.github.com>
2025-12-23 00:20:52 +0100

    Add tabs to filter timeline with filters functions

--- src/components/ResumeTimeline.tsx
+++ src/components/ResumeTimeline.tsx
@@ -1,4 +1,7 @@
+import Icon from "@mdi/react";
+import { ExperienceTypes } from "../enum/ExperienceTypes";
 import { TimelineItem } from "../interfaces/TimelineItem";
+import { mdiBagPersonalOutline, mdiBriefcaseVariantOutline, mdiHandHeartOutline, mdiSchoolOutline } from "@mdi/js";
 
 type ResumeTimelineProps = {
     items: TimelineItem[];
@@ -12,6 +15,27 @@ function ResumeTimeline(props: ResumeTimelineProps) {
                     <div key={key} className="timeline-item">
                         <div className="timeline-marker"></div>
                         <div className="timeline-content">
+                            <div className="timeline-tag is-icon">
+                                <figure className="image is-32x32">
+                                    {
+                                        item.type === ExperienceTypes.WORK ? (
+                                            <Icon path={mdiBriefcaseVariantOutline} size={1} />
+                                        ) :
+                                        item.type === ExperienceTypes.EDUCATION ? (
+                                            <Icon path={mdiSchoolOutline} size={1} />
+                                        ) :
+                                        item.type === ExperienceTypes.PERSONAL ? (
+                                            <Icon path={mdiBagPersonalOutline} size={1} />
+                                        ) :
+                                        item.type === ExperienceTypes.VOLUNTEER ? (
+                                            <Icon path={mdiHandHeartOutline} size={1} />
+                                        ) :
+                                        (
+                                            <Icon path={mdiBagPersonalOutline} size={1} />
+                                        )
+                                    }
+                                </figure>
+                            </div>
                             <p className="heading">{item.period}</p>
                             <p className="is-size-5 has-text-weight-bold">{item.work}</p>
                             <p className="is-italic is-size-8 is-family-secondary">{item.location}</p>
--- src/pages/home/Resume.tsx
+++ src/pages/home/Resume.tsx
@@ -2,6 +2,9 @@ import { useTranslation } from 'react-i18next';
 import ResumeTimeline from '../../components/ResumeTimeline';
 import { ExperienceTypes } from '../../enum/ExperienceTypes';
 import { TimelineItem } from '../../interfaces/TimelineItem';
+import Icon from '@mdi/react';
+import { mdiBagPersonalOutline, mdiBriefcaseVariantOutline, mdiSchoolOutline, mdiTagTextOutline } from '@mdi/js';
+import { useState } from 'react';
 
 function Resume() {
     const { t } = useTranslation();
@@ -35,6 +38,16 @@ function Resume() {
             type: ExperienceTypes.EDUCATION
         }
     ];
+    const [filteredItems, setFilteredItems] = useState<TimelineItem[]>(timelineItems);
+    const [activeTab, setActiveTabs] = useState<string>("All");
+
+    function toggleTabs(name: string): void {
+        let $target = document.getElementById(activeTab);
+        $target?.classList.toggle('is-active');
+        $target = document.getElementById(name);
+        $target?.classList.toggle('is-active');
+        setActiveTabs(name);
+    }
 
     return (
         <section
@@ -51,10 +64,55 @@ function Resume() {
             </h2>
             <div className="columns">
                 <div className="column">
-                    {
-                        // TODO: Add tabs for Education/Work Experience
-                    }
-                    <ResumeTimeline items={timelineItems}/>
+                    <div className="tabs is-left">
+                        <ul>
+                            <li id='All' className="is-active">
+                            <a onClick={() => {
+                                setFilteredItems(timelineItems);
+                                toggleTabs('All');
+                            }}>
+                                <span className="icon is-small">
+                                    <Icon path={mdiTagTextOutline} size={1} />
+                                </span>
+                                <span>{t('resume.tag1')}</span>
+                            </a>
+                            </li>
+                            <li id='Work'>
+                            <a onClick={() => {
+                                setFilteredItems(timelineItems.filter((item) => item.type == ExperienceTypes.WORK));
+                                toggleTabs('Work');
+                            }}>
+                                <span className="icon is-small">
+                                    <Icon path={mdiBriefcaseVariantOutline} size={1} />
+                                </span>
+                                <span>{t('resume.tag2')}</span>
+                            </a>
+                            </li>
+                            <li id='Education'>
+                            <a onClick={() => {
+                                setFilteredItems(timelineItems.filter((item) => item.type == ExperienceTypes.EDUCATION));
+                                toggleTabs('Education');
+                            }}>
+                                <span className="icon is-small">
+                                    <Icon path={mdiSchoolOutline} size={1} />
+                                </span>
+                                <span>{t('resume.tag3')}</span>
+                            </a>
+                            </li>
+                            <li id='Personal'>
+                            <a onClick={() => {
+                                setFilteredItems(timelineItems.filter((item) => item.type == ExperienceTypes.PERSONAL));
+                                toggleTabs('Personal');
+                            }}>
+                                <span className="icon is-small">
+                                    <Icon path={mdiBagPersonalOutline} size={1} />
+                                </span>
+                                <span>{t('resume.tag4')}</span>
+                            </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <ResumeTimeline items={filteredItems}/>
                 </div>
                 <div className="column">
                     {

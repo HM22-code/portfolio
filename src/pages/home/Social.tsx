7e0ffc4c0cafcf10e7af7f1312c5a3d80601355f
HM22-code <83293398+HM22-code@users.noreply.github.com>
2025-12-23 00:22:18 +0100

    Update social section with social items

--- src/pages/home/Social.tsx
+++ src/pages/home/Social.tsx
@@ -1,43 +1,51 @@
 import Icon from '@mdi/react';
 import { mdiLinkedin, mdiGithub, mdiGmail, mdiController } from '@mdi/js';
+import { SocialItem } from '../../interfaces/SocialItem';
+import { useTranslation } from 'react-i18next';
 
 function Social() {
+    const { t } = useTranslation();
+    const socials: SocialItem[] = [
+        {
+            link: "https://www.linkedin.com/in/hugo-montandon/",
+            icon: mdiLinkedin,
+            label: t('social.item1')
+        },
+        {
+            link: "https://github.com/HM22-code",
+            icon: mdiGithub,
+            label: t('social.item2')
+        },
+        {
+            link: "mailto:hugo.montandon2@gmail.com",
+            icon: mdiGmail,
+            label: t('social.item3')
+        },
+        {
+            link: "https://jude-erdrick.itch.io/",
+            icon: mdiController,
+            label: t('social.item4')
+        }
+    ];
 
     return (
         <section id="social" className="section level is-mobile">
             <div className='level is-mobile'>
-                <a href="https://www.linkedin.com/in/hugo-montandon/" className='level-item link is-primary'
-                    data-aos="fade-left"
-                    data-aos-delay="50"
-                    data-aos-duration="1000"
-                    data-aos-easing="ease-in-out"
-                >
-                    <Icon path={mdiLinkedin} title="Linkedin" size={2} />
-                </a>
-                <a href="https://github.com/HM22-code" className='level-item link is-primary'
-                    data-aos="fade-left"
-                    data-aos-delay="50"
-                    data-aos-duration="1000"
-                    data-aos-easing="ease-in-out"
-                >
-                    <Icon path={mdiGithub} title="Github" size={2} />
-                </a>
-                <a href="mailto:hugo.montandon2@gmail.com" className='level-item link is-primary'
-                    data-aos="fade-left"
-                    data-aos-delay="50"
-                    data-aos-duration="1000"
-                    data-aos-easing="ease-in-out"
-                >
-                    <Icon path={mdiGmail} title="Gmail" size={2} />
-                </a>
-                <a href="https://jude-erdrick.itch.io/" className='level-item link is-primary'
-                    data-aos="fade-left"
-                    data-aos-delay="50"
-                    data-aos-duration="1000"
-                    data-aos-easing="ease-in-out"
-                >
-                    <Icon path={mdiController} title="Itch.io" size={2} />
-                </a>
+                {
+                    socials.map((social, key) => (
+                        <a href={social.link} key={key} className='level-item link is-primary'
+                            data-aos="fade-left"
+                            data-aos-delay="50"
+                            data-aos-duration="1000"
+                            data-aos-easing="ease-in-out"
+                        >
+                            <div className='is-flex is-flex-direction-column is-justify-content-center is-align-content-center'>
+                                <Icon path={social.icon} title={social.label} size={2} />
+                                <p className='is-size-7 is-family-secondary has-text-centered'>{social.label}</p>
+                            </div>
+                        </a>
+                    ))
+                }
             </div>
         </section>
     )

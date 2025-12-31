83445553708862435dfd3d362fa49d4ed08f9129
HM22-code <83293398+HM22-code@users.noreply.github.com>
2025-12-31 13:52:37 +0100

    Add social links to Footer component

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,10 +2,35 @@ import reactLogo from "/assets/svg/react.svg";
 import viteLogo from "/assets/svg/vite.svg";
 import bulmaLogo from "/assets/svg/bulma.svg";
 import { useTranslation } from 'react-i18next';
+import { SocialItem } from "../../interfaces/SocialItem";
+import { mdiController, mdiGithub, mdiGmail, mdiLinkedin } from "@mdi/js";
+import Icon from "@mdi/react";
 
 // Footer component
 function Footer() {
     const { t } = useTranslation();
+    const socialLinks: SocialItem[] = [
+        {
+                link: "https://www.linkedin.com/in/hugo-montandon/",
+                icon: mdiLinkedin,
+                label: t('social.item1')
+            },
+            {
+                link: "https://github.com/HM22-code",
+                icon: mdiGithub,
+                label: t('social.item2')
+            },
+            {
+                link: "mailto:hugo.montandon2@gmail.com",
+                icon: mdiGmail,
+                label: t('social.item3')
+            },
+            {
+                link: "https://jude-erdrick.itch.io/",
+                icon: mdiController,
+                label: t('social.item4')
+            }
+    ];
 
     return (
         <footer className="footer is-flex is-flex-direction-column py-4">
@@ -13,8 +38,27 @@ function Footer() {
                 <p className="level-item is-family-code">{t('footer.title')}</p>
             </div>
             <br/>
+            <div className="columns">
+                <div className="column">
+                    <div className="level is-mobile">
+                        <div className="level-left">
+                            {
+                                socialLinks.map((social, key) => (
+                                    <a href={social.link} key={key} className="level-item">
+                                        <div className="is-flex is-flex-direction-row is-align-items-center">
+                                            <Icon path={social.icon} title={social.label} size={1} />
+                                            <span>{social.label}</span>
+                                        </div>
+                                    </a>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
             <div className="level is-mobile">
-                <div className="level-left">
+                <div className="level-left"></div>
+                <div className="level-right">
                     <a href="https://vitejs.dev" target="_blank" className="level-item">
                         <img src={viteLogo} className="image logo vite" alt="Vite logo" />
                     </a>
@@ -25,6 +69,8 @@ function Footer() {
                         <img src={reactLogo} className="image logo react" alt="React logo" />
                     </a>
                 </div>
+                </div>
+            </div>
             </div>
         </footer>
     )

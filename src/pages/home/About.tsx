2bd50b052c61343ecac564c98fb88ed1b8af9efc
HM22-code <83293398+HM22-code@users.noreply.github.com>
2025-12-31 18:26:33 +0100

    Add content in about section

--- src/pages/home/About.tsx
+++ src/pages/home/About.tsx
@@ -1,3 +1,5 @@
+import { mdiCellphoneLink, mdiGamepadVariantOutline, mdiPaletteOutline, mdiWeb } from '@mdi/js';
+import Icon from '@mdi/react';
 import { useTranslation } from 'react-i18next';
 
 // About section
@@ -10,12 +12,18 @@ function About() {
             className="section"
             data-aos="fade-up"
             data-aos-offset="200"
-            data-aos-delay="50"
-            data-aos-duration="1000"
+            data-aos-delay="0"
+            data-aos-duration="500"
             data-aos-easing="ease-in-out"
         >
             <div className='columns'>
-                <div className='column'>
+                <div className='column'
+                    data-aos="fade-up"
+                    data-aos-offset="200"
+                    data-aos-delay="0"
+                    data-aos-duration="750"
+                    data-aos-easing="ease-in-out"
+                >
                     <h2 className="title is-4">
                         {t('about.title')}
                     </h2>
@@ -23,10 +31,60 @@ function About() {
                         {t('about.description')}
                     </p>
                 </div>
-                <div className='column'>
-                    {
-                        // TODO: Place for additional content
-                    }
+                <div className='column'
+                    data-aos="fade-up"
+                    data-aos-offset="200"
+                    data-aos-delay="0"
+                    data-aos-duration="750"
+                    data-aos-easing="ease-in-out"
+                >
+                    <h2 className="title is-4">
+                        {t('about.title2')}
+                    </h2>
+                    <div className='content'>
+                        <div className="columns is-multiline is-half-tablet">
+                            <div className="column is-half">
+                                <div className="box is-flex is-flex-direction-row is-align-items-center">
+                                    <span className="icon mr-2">
+                                        <Icon path={mdiCellphoneLink} title="App Icon" size={2} />
+                                    </span>
+                                    <p className='title is-7'>
+                                        {t('about.do1')}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="column is-half">
+                                <div className="box is-flex is-flex-direction-row is-align-items-center">
+                                    <span className="icon mr-2">
+                                        <Icon path={mdiWeb} title="Web Icon" size={2} />
+                                    </span>
+                                    <p className='title is-7'>
+                                        {t('about.do2')}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="column is-half">
+                                <div className="box is-flex is-flex-direction-row is-align-items-center">
+                                    <span className="icon mr-2">
+                                        <Icon path={mdiGamepadVariantOutline} title="Game Icon" size={2} />
+                                    </span>
+                                    <p className='title is-7'>
+                                        {t('about.do3')}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="column is-half">
+                                <div className="box is-flex is-flex-direction-row is-align-items-center">
+                                    <span className="icon mr-2">
+                                        <Icon path={mdiPaletteOutline} title="Graphic Icon" size={2} />
+                                    </span>
+                                    <p className='title is-7'>
+                                        {t('about.do4')}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
